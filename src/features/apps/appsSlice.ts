92923b7b2761e7ace189eee7bb0759e7c2a370cf
Yarin Mizrachi <yarin@beprofit.co>
2024-05-17 18:51:10 +0300

    improved cases functions

--- src/features/apps/appsSlice.ts
+++ src/features/apps/appsSlice.ts
@@ -43,9 +43,17 @@ export const fetchAllApps = createAsyncThunk("apps/fetchAllApps", async () => {
     const results = await api.getAllApps();
     return results;
   } catch (error) {
-    throw new Error("Error fetching top paid apps");
+    throw new Error("Error fetching all apps");
   }
 });
+
+const handlePending = (state: AppState) => {
+  state.status = "loading";
+};
+
+const handleRejected = (state: AppState) => {
+  state.status = "failed";
+};
 
 const appsSlice = createSlice({
   name: "apps",
@@ -53,37 +61,25 @@ const appsSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     builder
-      .addCase(fetchTopFreeApps.pending, (state) => {
-        state.status = "loading";
-      })
+      .addCase(fetchTopFreeApps.pending, handlePending)
+      .addCase(fetchTopFreeApps.rejected, handleRejected)
       .addCase(fetchTopFreeApps.fulfilled, (state, action) => {
         state.status = "idle";
         state.topFreeApps = action.payload;
       })
-      .addCase(fetchTopFreeApps.rejected, (state) => {
-        state.status = "failed";
-      })
-      .addCase(fetchTopPaidApps.pending, (state) => {
-        state.status = "loading";
-      })
+      .addCase(fetchTopPaidApps.pending, handlePending)
+      .addCase(fetchTopPaidApps.rejected, handleRejected)
       .addCase(fetchTopPaidApps.fulfilled, (state, action) => {
         state.status = "idle";
         state.topPaidApps = action.payload;
       })
-      .addCase(fetchTopPaidApps.rejected, (state) => {
-        state.status = "failed";
-      })
-      .addCase(fetchAllApps.pending, (state) => {
-        state.status = "loading";
-      })
+      .addCase(fetchAllApps.pending, handlePending)
+      .addCase(fetchAllApps.rejected, handleRejected)
       .addCase(fetchAllApps.fulfilled, (state, action) => {
         state.status = "idle";
         const { freeResponse, paidResponse } = action.payload;
-        state.topPaidApps = paidResponse;
         state.topFreeApps = freeResponse;
-      })
-      .addCase(fetchAllApps.rejected, (state) => {
-        state.status = "failed";
+        state.topPaidApps = paidResponse;
       });
   },
 });
