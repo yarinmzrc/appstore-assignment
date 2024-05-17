aa6f2c6351a6153ae010ff51f3fec64dc1d5c4ab
Yarin Mizrachi <yarin@beprofit.co>
2024-05-17 14:59:46 +0300

    added heart icons

--- src/components/AppCard.tsx
+++ src/components/AppCard.tsx
@@ -1,22 +1,52 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import React from "react";
 import { Card, CardContent } from "@/components/ui/card";
+import { HeartIcon, HeartFilledIcon } from "@radix-ui/react-icons";
+import { useAppDispatch, useAppSelector } from "@/hooks";
+import { RootState } from "@/store";
+import {
+  addFavorite,
+  removeFavorite,
+} from "@/features/favorites/favoritesSlice";
 
 interface AppCardProps {
   app: any;
 }
 
 const AppCard: React.FC<AppCardProps> = ({ app }) => {
+  const dispatch = useAppDispatch();
+
+  const { favoriteApps } = useAppSelector(
+    (state: RootState) => state.favorites
+  );
+
+  const isFavorite = favoriteApps.some(
+    (favoriteApp) => favoriteApp.id === app.id
+  );
+
+  const handleFavoriteClick = () => {
+    if (isFavorite) {
+      dispatch(removeFavorite(app));
+    } else {
+      dispatch(addFavorite(app));
+    }
+  };
+
   return (
-    <Card className="min-w-[300px]">
-      <CardContent className="p-5">
-        <img
-          width="50px"
-          height="50px"
-          className="rounded-lg mb-1"
-          src={app.artworkUrl100}
-          alt={app.name}
-        />
+    <Card className="min-w-[300px] dark:border dark:border-slate-600">
+      <CardContent className="p-5 dark:bg-slate-900 dark:rounded-lg">
+        <div className="flex justify-between items-start">
+          <img
+            width="50px"
+            height="50px"
+            className="rounded-lg mb-1"
+            src={app.artworkUrl100}
+            alt={app.name}
+          />
+          <button onClick={handleFavoriteClick}>
+            {isFavorite ? <HeartFilledIcon /> : <HeartIcon />}
+          </button>
+        </div>
         <p className="text-xs text-gray-400">{app.name}</p>
         <div>{app.artistName}</div>
       </CardContent>
