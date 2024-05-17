e9170aeb00c071f5336aaddd4fe363f8909d9d98
Yarin Mizrachi <yarin@beprofit.co>
2024-05-17 13:41:59 +0300

    added shadcn + dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./AppRoutes";
 import NavBar from "./components/NavBar";
+import { ThemeProvider } from "@/components/theme-provider";
 
 function App() {
   return (
-    <Router>
-      <div className="app">
-        <NavBar />
-        <AppRoutes />
-      </div>
-    </Router>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <div className="bg-gray-200 dark:bg-slate-800 dark:text-white">
+          <div className="container max-w-[1360px] min-h-screen ">
+            <NavBar />
+            <AppRoutes />
+          </div>
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom";
+import { ModeToggle } from "./ModeToggle";
 
 const NavBar = () => {
   return (
-    <div className="flex justify-between items-center">
-      <p>AppStore</p>
-      <div className="flex items-center gap-2">
-        <Link to="/">Home</Link>
-        <Link to="/favorites">Favorites</Link>
+    <div className="flex justify-between items-center py-3">
+      <p className="font-bold text-3xl">AppStore</p>
+      <div className="flex items-center gap-3">
+        <Link to="/" className="hover:underline">
+          Home
+        </Link>
+        <Link to="/favorites" className="hover:underline">
+          Favorites
+        </Link>
       </div>
+      <ModeToggle />
     </div>
   );
 };
