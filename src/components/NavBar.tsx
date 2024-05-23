5258a6e27a87a5e34e079010dddca1fe68b2483d
Yarin Mizrachi <yarin@beprofit.co>
2024-05-23 10:29:11 +0300

    improved NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,18 @@
 import { Link, useLocation } from "react-router-dom";
 import { ModeToggle } from "./ModeToggle";
 
+interface NavLinkProps {
+  to: string;
+  text: string;
+  isActive: boolean;
+}
+
+const NavLink = ({ to, text, isActive }: NavLinkProps) => (
+  <Link to={to} className={`hover:underline ${isActive ? "underline" : ""}`}>
+    {text}
+  </Link>
+);
+
 const NavBar = () => {
   const location = useLocation();
   const path = location.pathname;
@@ -9,20 +21,12 @@ const NavBar = () => {
     <div className="flex justify-between items-end py-4 mb-6 border-b border-slate-300 dark:border-slate-600">
       <p className="font-bold text-2xl">AppStore</p>
       <div className="flex items-center gap-3">
-        <Link
-          to="/"
-          className={`hover:underline ${path === "/" ? "underline" : ""}`}
-        >
-          Home
-        </Link>
-        <Link
+        <NavLink to="/" text="Home" isActive={path === "/"} />
+        <NavLink
           to="/favorites"
-          className={`hover:underline ${
-            path === "/favorites" ? "underline" : ""
-          }`}
-        >
-          Favorites
-        </Link>
+          text="Favorites"
+          isActive={path === "/favorites"}
+        />
       </div>
       <ModeToggle />
     </div>
