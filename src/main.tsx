7a12c529e3aa60f2cd4169ba1e957a7690a79ff6
Yarin Mizrachi <yarin@beprofit.co>
2024-05-23 09:52:51 +0300

    added strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,14 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { StrictMode } from "react";
 import { Provider } from "react-redux";
+import App from "./App.tsx";
 import { store } from "./store/index.ts";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
 );
