d1d48a4b512a685bb15c5297290be42c73dfd68a
Yarin Mizrachi <yarin@beprofit.co>
2024-05-23 15:24:12 +0300

    added types to api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,47 +1,52 @@
+import { AppData } from "@/types";
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_BASE_URL as string,
 });
 
-const api = {
-  getTopFreeApps: async () => {
-    try {
-      const response = await axiosInstance.get(
-        "/api/v2/us/apps/top-free/10/apps.json"
-      );
-      return response?.data?.feed?.results || [];
-    } catch (error) {
-      console.error("Error fetching top free apps:", error);
-      throw error;
-    }
-  },
-  getTopPaidApps: async () => {
-    try {
-      const response = await axiosInstance.get(
-        "/api/v2/us/apps/top-paid/25/apps.json"
-      );
-      return response?.data?.feed?.results || [];
-    } catch (error) {
-      console.error("Error fetching top paid apps:", error);
-      throw error;
-    }
-  },
-  getAllApps: async () => {
-    try {
-      const [freeResponse, paidResponse] = await Promise.all([
-        api.getTopFreeApps(),
-        api.getTopPaidApps(),
-      ]);
+interface AllAppsResponse {
+  freeResponse: Array<AppData>;
+  paidResponse: Array<AppData>;
+}
 
-      return {
-        freeResponse,
-        paidResponse,
-      };
-    } catch (error) {
-      console.error("Error fetching apps:", error);
-      throw error;
+const api = {
+  getTopFreeApps: async (): Promise<Array<AppData>> => {
+    const response = await axiosInstance.get(
+      "/api/v2/us/apps/top-free/10/apps.json"
+    );
+
+    if (response.status === 200) {
+      return response.data.feed.results;
     }
+
+    return [];
+  },
+  getTopPaidApps: async (): Promise<Array<AppData>> => {
+    const response = await axiosInstance.get(
+      "/api/v2/us/apps/top-paid/25/apps.json"
+    );
+
+    if (response.status === 200) {
+      return response.data.feed.results;
+    }
+
+    return [];
+  },
+  getAllApps: async (): Promise<AllAppsResponse> => {
+    const [freeResponse, paidResponse] = await Promise.all([
+      axiosInstance.get("/api/v2/us/apps/top-free/10/apps.json"),
+      axiosInstance.get("/api/v2/us/apps/top-paid/25/apps.json"),
+    ]);
+
+    if (freeResponse.status === 200 || paidResponse.status === 200) {
+      return {
+        freeResponse: freeResponse.data?.feed?.results || [],
+        paidResponse: paidResponse.data?.feed?.results || [],
+      };
+    }
+
+    return { freeResponse: [], paidResponse: [] };
   },
 };
 
